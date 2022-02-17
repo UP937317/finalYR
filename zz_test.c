f6e127c7ff9b2822adbb57f6826cc016df633d15
Jan <up937317@myport.ac.uk>
2022-02-17 12:38:06 +0000

    finished adjecancy matrices, its n powers

--- zz_test.c
+++ zz_test.c
@@ -3,12 +3,12 @@
 #include <string.h>
 #include "gb_graph.h"  
 
+#define NUMOFVERTICES 12
 
 Area s;
-int numOfVertices = 12;
 Vertex *arr_vert[12];
 
-/* reverse search and its functions*/
+// reverse search and its functions
 
 typedef int perm[100];
 void Adj (perm v, int i) /* adjacency oracle */{}
@@ -37,25 +37,104 @@ int reversesearch (perm v, int maxdeg){
 	return count;
 }
 
+/* //MATRICES WITH POINTERS TEST FUNCTIONS - ADD LATER
+
+// int** testAdjMatrix = createMatrix(NUMOFVERTICES,NUMOFVERTICES);
+
+int** createMatrix(int m, int n){
+	int* values = calloc(m*n, sizeof(int));
+    int** rows = malloc(m*sizeof(int*));
+    for (int i=0; i<m; ++i){
+        rows[i] = values + i*n;
+    }
+    return rows;
+}
+
+void printTESTmatrix(const int** mat[NUMOFVERTICES][NUMOFVERTICES]){
+	for(int i = 0; i< NUMOFVERTICES; i++){
+		for(int j = 0; j< NUMOFVERTICES ; j++){
+			printf("%d ", mat[i][j]);
+		}
+		printf("\n");
+	}
+}*/
+
+//initiaze matrix to all 0s
+void initiateMatrix(int mat1[NUMOFVERTICES][NUMOFVERTICES]){
+	for(int k = 0; k < NUMOFVERTICES; k++){
+		for(int l = 0; l < NUMOFVERTICES; l++){
+			mat1[k][l] = 0;
+		}
+	}
+
+}
+
+//squares the matrix to the nth power
+void squareMatrix(int mat1[NUMOFVERTICES][NUMOFVERTICES], int result[NUMOFVERTICES][NUMOFVERTICES], int n){
+	int temp[NUMOFVERTICES][NUMOFVERTICES];
+	int sum = 0;
+
+	for ( int b = 0 ; b < NUMOFVERTICES ; b++ ) {
+        for ( int d = 0 ; d < NUMOFVERTICES ; d++ ) {
+            result[b][d] = mat1[b][d];
+        }
+    }
+
+	for (int l = 0; l < n - 1; l++){
+
+		for (int i = 0; i < NUMOFVERTICES; i++) {
+	    	for (int j = 0; j < NUMOFVERTICES; j++) {
+	        	for (int k = 0; k < NUMOFVERTICES; k++) {
+	        		sum += mat1[i][k] * result[k][j];
+	        	}
+	        	temp[i][j] = sum;
+	        	sum = 0;
+	        }
+	    }
+	
+
+		for ( int b = 0 ; b < NUMOFVERTICES ; b++ ) {
+	        for ( int d = 0 ; d < NUMOFVERTICES ; d++ ) {
+	            result[b][d] = temp[b][d];
+	        }
+	    }
+	}
+}
+
+void printMatrix(int mat[NUMOFVERTICES][NUMOFVERTICES]){
+	for(int i = 0; i< NUMOFVERTICES; i++){
+		for(int j = 0; j< NUMOFVERTICES ; j++){
+			printf("%d ", mat[i][j]);
+		}
+		printf("\n");
+	}
+}
+
 int main(){
 
-	/*Constructing triangulated graph, 9-critical, reference 12.57.1 from Boutin paper*/
-
-	Graph *triang = gb_new_graph(12L);
+	//Constructing triangulated graph, 9-critical, reference 12.57.1 from Boutin paper
+	Graph *triang = gb_new_graph(NUMOFVERTICES);
 	arr_vert[0] = triang->vertices;
 
-	for (int i=1; i<13;i++){
+	for (int i=1; i<NUMOFVERTICES;i++){
 		arr_vert[i] = arr_vert[0] + i;
 	}
 
-	printf(triang->id);
-	printf("\n");
+	//printf(triang->id);
 
-	for (int i=0; i<13;i++){
-		arr_vert[i]->name = gb_save_string("v %d",i);
-		/*printf(arr_vert[i]->name);
-		printf("\n");*/
-	}
+	arr_vert[0]->name = gb_save_string("0");
+	arr_vert[1]->name = gb_save_string("1");
+	arr_vert[2]->name = gb_save_string("2");
+	arr_vert[3]->name = gb_save_string("3");
+	arr_vert[4]->name = gb_save_string("4");
+	arr_vert[5]->name = gb_save_string("5");
+	arr_vert[6]->name = gb_save_string("6");
+	arr_vert[7]->name = gb_save_string("7");
+	arr_vert[8]->name = gb_save_string("8");
+	arr_vert[9]->name = gb_save_string("9");
+	arr_vert[10]->name = gb_save_string("10");
+	arr_vert[11]->name = gb_save_string("11");
+
 
 	gb_new_edge(arr_vert[0],arr_vert[7],1L);
 	gb_new_edge(arr_vert[0],arr_vert[2],1L);
@@ -85,28 +164,51 @@ int main(){
 	gb_new_edge(arr_vert[5],arr_vert[6],1L);
 	gb_new_edge(arr_vert[8],arr_vert[4],1L);
 
-	/*Loop through all arcs*/
+	//adjecancy matrix for the graph
 
+	int adjecancyMatrix[NUMOFVERTICES][NUMOFVERTICES];
+
+	//populating adjecancy matrix
 	Vertex *v;
 	Arc *a;
-	/*for(int j = 0; j<13 ; j++){
+
+	//populating with dummy 0s
+	initiateMatrix(adjecancyMatrix);
+
+	//print the matrix for tdebugging
+	//printMatrix(adjecancyMatrix);
+
+	//pupulating matrix with 1s where there is an edge
+	for(int j = 0; j<NUMOFVERTICES ; j++){
 		for(a = arr_vert[j]->arcs; a; a = a->next){
-			printf(arr_vert[j]->name);
-			printf("\n");
-			printf(a);
-			printf("\n");
+			int temp = atoi(a->tip->name);
+			adjecancyMatrix[j][temp] = 1;
 		}
-	}*/
+	}
+	
+	//print the matrix for tdebugging
+	//printMatrix(adjecancyMatrix);	
+
+	// SQUARING THE MATRIX
+	int result[NUMOFVERTICES][NUMOFVERTICES];
+	initiateMatrix(result);
 
 
+	//square the matrix to the power of n and save it to result, print it
+	squareMatrix(adjecancyMatrix,result, 4);
+	printMatrix(result);
+	
+
+
+	printf("kundovina nefunguje");
 	int flippable = 0;
 
-	for(int i = 0; i<13 ; i++){
-		for(int j = 0; j<13 ; j++){
+	for(int i = 0; i<NUMOFVERTICES ; i++){
+		for(int j = 0; j<NUMOFVERTICES ; j++){
 			if(i == j){continue;}
-			for(int k = 0; k<13 ; k++){
+			for(int k = 0; k<NUMOFVERTICES ; k++){
 				if(k == i || k == j){continue;}
-				for(int l = 0; l<13 ; l++){
+				for(int l = 0; l<NUMOFVERTICES ; l++){
 					if(l == i || l == j || l == k){continue;}
 					for(a = arr_vert[i]->arcs; a; a = a->next){
 						if(a->tip == arr_vert[j]){
@@ -119,29 +221,41 @@ int main(){
 							flippable += 1;
 						}
 					}
-					for(a = arr_vert[j]->arcs; a; a = a->next){
+					/*for(a = arr_vert[j]->arcs; a; a = a->next){
 						if(a->tip == arr_vert[k]){
 							flippable += 1;
 						}
-					}
+					}*/
 					for(a = arr_vert[k]->arcs; a; a = a->next){
 						if(a->tip == arr_vert[l]){
 							flippable += 1;
 						}
 					}
-					if(flippable == 5){
-						/*printf("quadriteral: %s, %s, %s, %s is flipable", arr_vert[i]->name,arr_vert[j]->name,arr_vert[k]->name,arr_vert[l]->name);
-						*/
-						printf("quadriteral: %d, %d, %d, %d is theoretically flipable",i,j,k,l);
-						printf("\n");
+					if(flippable == 4){
+						printf("quadriteral: %s, %s, %s, %s is flipable\n", arr_vert[i]->name,arr_vert[j]->name,arr_vert[k]->name,arr_vert[l]->name);
+						//printf("flippable edge: %s, %s\n", arr_vert[i]->name,arr_vert[k]->name);
 					}
 					flippable = 0;
 				}
 			}
 		}
 	}
+	
 
-	/* Flips in triangularion*/
-	/*Output all flipable edges (all possible flips)*/
+
+	/*
+
+	a -> b
+	a -> c
+	a -> d
+	b -> c
+	b -> d
+	c -> d
+
+
+	*/
+	/*define flip function*/
+	/* eliminating valse positives */
+	/* flip two flippable edges and check numbers of paths_2 before and after*/
 
 }
