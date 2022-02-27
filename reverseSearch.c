86b150b19c5687a0a975e63fa411799c3cb9cafe
Jan <up937317@myport.ac.uk>
2022-02-27 15:56:25 +0000

    restoring graph from file rather then building it in the main file

--- reverseSearch.c
+++ reverseSearch.c
@@ -306,7 +306,7 @@ void makeFlipList(){
 int main(){
 
 	//Constructing triangulated graph, 9-critical, reference 12.57.1 from Boutin paper
-	Graph *triang = gb_new_graph(NUMOFVERTICES);
+	Graph *triang = restore_graph("triang.gb");
 	arr_vert[0] = triang->vertices;
 
 	for (int i=1; i<NUMOFVERTICES;i++){
@@ -314,49 +314,6 @@ int main(){
 	}
 
 	//printf(triang->id);
-
-	arr_vert[0]->name = gb_save_string("0");
-	arr_vert[1]->name = gb_save_string("1");
-	arr_vert[2]->name = gb_save_string("2");
-	arr_vert[3]->name = gb_save_string("3");
-	arr_vert[4]->name = gb_save_string("4");
-	arr_vert[5]->name = gb_save_string("5");
-	arr_vert[6]->name = gb_save_string("6");
-	arr_vert[7]->name = gb_save_string("7");
-	arr_vert[8]->name = gb_save_string("8");
-	arr_vert[9]->name = gb_save_string("9");
-	arr_vert[10]->name = gb_save_string("10");
-	arr_vert[11]->name = gb_save_string("11");
-
-
-	gb_new_edge(arr_vert[0],arr_vert[7],1L);
-	gb_new_edge(arr_vert[0],arr_vert[2],1L);
-	gb_new_edge(arr_vert[0],arr_vert[3],1L);
-	gb_new_edge(arr_vert[3],arr_vert[7],1L);
-	gb_new_edge(arr_vert[3],arr_vert[2],1L);
-	gb_new_edge(arr_vert[3],arr_vert[11],1L);
-	gb_new_edge(arr_vert[3],arr_vert[8],1L);
-	gb_new_edge(arr_vert[3],arr_vert[5],1L);
-	gb_new_edge(arr_vert[3],arr_vert[6],1L);
-	gb_new_edge(arr_vert[3],arr_vert[1],1L);
-	gb_new_edge(arr_vert[1],arr_vert[6],1L);
-	gb_new_edge(arr_vert[1],arr_vert[5],1L);
-	gb_new_edge(arr_vert[1],arr_vert[8],1L);
-	gb_new_edge(arr_vert[1],arr_vert[9],1L);
-	gb_new_edge(arr_vert[1],arr_vert[2],1L);
-	gb_new_edge(arr_vert[1],arr_vert[10],1L);
-	gb_new_edge(arr_vert[10],arr_vert[7],1L);
-	gb_new_edge(arr_vert[10],arr_vert[2],1L);
-	gb_new_edge(arr_vert[11],arr_vert[2],1L);
-	gb_new_edge(arr_vert[11],arr_vert[4],1L);
-	gb_new_edge(arr_vert[11],arr_vert[8],1L);
-	gb_new_edge(arr_vert[11],arr_vert[9],1L);
-	gb_new_edge(arr_vert[9],arr_vert[2],1L);
-	gb_new_edge(arr_vert[9],arr_vert[4],1L);
-	gb_new_edge(arr_vert[9],arr_vert[8],1L);
-	gb_new_edge(arr_vert[5],arr_vert[6],1L);
-	gb_new_edge(arr_vert[8],arr_vert[4],1L);
-	gb_new_edge(arr_vert[2],arr_vert[7],1L);
 
 	//adjecancy matrix for the graph
 	int adjecancyMatrix[NUMOFVERTICES][NUMOFVERTICES];
